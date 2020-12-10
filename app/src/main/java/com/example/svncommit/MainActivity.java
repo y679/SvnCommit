60386b95ad533277062747f1b7d797e7d48c9768
suyuan <suyuan2022@163.com>
2020-12-10 16:58:27 +0800

    对的

--- app/src/main/java/com/example/svncommit/MainActivity.java
+++ app/src/main/java/com/example/svncommit/MainActivity.java
@@ -8,5 +8,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         String str = "Git第一次上传文件1111111";
+        String a="";
     }
 }
