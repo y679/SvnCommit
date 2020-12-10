90af0a52997a075d6f81a4f5e8f0d8c24a27685d
suyuan <suyuan2022@163.com>
2020-12-10 16:50:28 +0800

    Initial commit

---
+++ app/src/main/java/com/example/svncommit/MainActivity.java
@@ -0,0 +1,12 @@
+package com.example.svncommit;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        String str = "Git第一次上传文件";
+    }
+}
